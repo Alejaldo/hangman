4eb2981fa1af4561aa16d3936cbad145aa29d7ba
Alejaldo <statfluct@yahoo.com>
2020-12-31 20:19:45 +0500

    correction

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,6 @@
 arr = File.open("5desk.txt").readlines.each do |line|
-  line.strip
+  clean_line = line.to_s.gsub(/[^a-zA-Z]/,"")
+  clean_line.downcase! if clean_line.size.between?(5,12)
 end
+
 puts arr.sample
