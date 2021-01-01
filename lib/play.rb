43aa8b6f60f3d903cf146d081dbb4c98632090b0
Alejaldo <statfluct@yahoo.com>
2021-01-02 01:30:11 +0500

    Working code

--- lib/play.rb
+++ lib/play.rb
@@ -1,69 +1,79 @@
 class Hangman
 
   def initialize
-    @letters = ('a'..'z').to_a
     @word = words.sample
-    @lives = 7
-    @correct_guesses = []
+    @lives = 6
     @word_teaser = ""
 
-    @word.first.size.times do
-      word_teaser += "_ "
+    @word.size.times do
+      @word_teaser += "_ "
     end
   end
 
   def words
-    [
-      ["cricket", "first"],
-      ["jogging", "second"],
-      ["celebrate", "third"],
-      ["continent", "fourth"],
-      ["exotic", "fifth"],
-    ]
+    File.open("5desk.txt").readlines.each do |line|
+      clean_line = line.to_s.gsub(/[^a-zA-Z]/,"")
+      clean_line.downcase! if clean_line.size.between?(5,12)
+    end
   end
 
-  def print_teaser last_guess
-    word_teaser = ""
+  def print_teaser last_guess = nil
+    update_teaser(last_guess) unless last_guess.nil?
+    puts @word_teaser
+  end
 
-    update_teaser
+  def update_teaser last_guess
+    new_teaser = @word_teaser.split
 
-    puts word_teaser
+    new_teaser.each_with_index do |letter, index|
+      # replace blank values with guessed letter if matches in word
+      if letter == '_' && @word[index] == last_guess
+        new_teaser[index] = last_guess
+      end
+
+    end
+
+    @word_teaser = new_teaser.join(' ')
   end
 
   def make_guess
+
     if @lives > 0
       puts "Enter a letter"
       guess = gets.chomp
 
-      good_guess = @word.first.include? guess
+      good_guess = @word.include? guess
 
-      if good_guess
+      if guess == "exit"
+        puts "Thank you for playing"
+      elsif good_guess
         puts "Great match!"
 
-        @correct_guesses << guess
-
-        # remove correct guess from alphabet
-        @letters.delete guess
-
         print_teaser guess
-        make_guess
+
+        if @word == @word_teaser.split.join
+          puts "You are lucky Joe gdmn mzfk!"
+        else
+          make_guess
+        end
       else
         @lives -= 1
-        puts "Wrong! You have #{ @lives }. Try again!"
+        puts "Wrong! You have #{ @lives } lives left. Try again!"
         make_guess
       end
+
     else
       puts "Game over!"
     end
+
   end
 
   def begin
     # ask user for a letter
-    puts "New game started ... your word is #{ @word.first.size } characters long" 
+    puts "New game started ... your word is #{ @word.size } characters long" 
+    puts "To exit game at any point type 'exit'"
     print_teaser
-
-    puts "Clue: #{ @word.last }"
-
+    puts
     make_guess
   end
 
